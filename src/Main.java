d94e25b23e56fbed4bb3e34815a97296f2ffed78
Vladimir <zdotavv@ukr.net>
2022-03-18 14:09:23 +0200

    AddTigers

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Попугаи");
         System.out.println("Ящерицы");
         System.out.println("Слоны");
+        System.out.println("Тигры");
     }
 }
