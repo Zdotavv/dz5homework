c054a1185d9413910a4f695185ed39c9830deb55
Vladimir <zdotavv@ukr.net>
2022-03-18 13:18:12 +0200

    add animals

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Список животных в приюте: ");
+        System.out.println("Коты");
+        System.out.println("Собаки");
+        System.out.println("Хомяки");
+        System.out.println("Попугаи");
     }
 }
